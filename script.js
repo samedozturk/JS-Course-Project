5bb5bd205513972aed311ce50487c4abce67c1db
samedozturk <abdulsamedozturk00830@gmail.com>
2024-08-24 22:20:27 +0300

    dark mod ekledim ve sayfayı yukarıya taşıyan bir buton ekledim

--- script.js
+++ script.js
@@ -58,9 +58,44 @@ function activeButton(y){
 }
 
 const sidebar = document.querySelector(".sidebar");
-const btn = document.querySelector("#menu-btn");
+const btn = document.querySelector("#menu-btn1");
 window.addEventListener("click",function (event){
     if(!sidebar.contains(event.target) && !btn.contains(event.target)){
         sidebar.style.width = "0";
     }
 })
+function bactotop(){
+    window.scrollTo(0, 0);
+}
+let localdarkmode = "false";
+let themebtn = document.getElementById("theme-btn");
+document.addEventListener("DOMContentLoaded", function (){
+    localdarkmode = localStorage.getItem("dark-mode");
+    if(localdarkmode === "true"){
+        themebtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M440-800v-120h80v120h-80Zm0 760v-120h80v120h-80Zm360-400v-80h120v80H800Zm-760 0v-80h120v80H40Zm708-252-56-56 70-72 58 58-72 70ZM198-140l-58-58 72-70 56 56-70 72Zm564 0-70-72 56-56 72 70-58 58ZM212-692l-72-70 58-58 70 72-56 56Zm268 452q-100 0-170-70t-70-170q0-100 70-170t170-70q100 0 170 70t70 170q0 100-70 170t-170 70Zm0-80q67 0 113.5-46.5T640-480q0-67-46.5-113.5T480-640q-67 0-113.5 46.5T320-480q0 67 46.5 113.5T480-320Zm0-160Z"/></svg>`
+        themebtn.classList.toggle("dark-mode", localdarkmode === "true");
+    }
+    else{
+        themebtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z"/></svg>`
+        themebtn.classList.toggle("dark-mode", localdarkmode === "true");
+    }
+})
+
+function changetheme(){
+    if(themebtn.classList.contains("dark-mode")){
+        // aydınlıga geçiş
+        themebtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z"/></svg>`
+        localStorage.setItem("dark-mode", "false");
+        themebtn.classList.toggle("dark-mode");
+        document.body.classList.remove("dark-mode");
+    }else{
+        // karanliga geçiş
+        themebtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M440-800v-120h80v120h-80Zm0 760v-120h80v120h-80Zm360-400v-80h120v80H800Zm-760 0v-80h120v80H40Zm708-252-56-56 70-72 58 58-72 70ZM198-140l-58-58 72-70 56 56-70 72Zm564 0-70-72 56-56 72 70-58 58ZM212-692l-72-70 58-58 70 72-56 56Zm268 452q-100 0-170-70t-70-170q0-100 70-170t170-70q100 0 170 70t70 170q0 100-70 170t-170 70Zm0-80q67 0 113.5-46.5T640-480q0-67-46.5-113.5T480-640q-67 0-113.5 46.5T320-480q0 67 46.5 113.5T480-320Zm0-160Z"/></svg>`
+        themebtn.classList.toggle("dark-mode");
+        localStorage.setItem("dark-mode", "true");
+        document.body.classList.add("dark-mode");
+    }
+}
+
+
+
